22a6cef9b59ec4ef78bfb90b3fa54c99f8602348
Abirami1213 <abirami@yectra.com>
2024-06-03 15:45:00 +0530

    Final change

--- function_app.py
+++ function_app.py
@@ -20,6 +20,6 @@ def fn_testinggithubactions(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            "Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
